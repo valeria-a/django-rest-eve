bc8642a77e0a354e2ae6c249f4363dd8c0218c15
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2023-03-22 20:13:56 +0200

    initial commit

--- movies_rest_app/serializers.py
+++ movies_rest_app/serializers.py
@@ -1,5 +1,31 @@
 from rest_framework import serializers
 
+from movies_rest_app.models import Movie, Actor
 
-class MovieSerializer(serializers.Serializer):
 
+class ActorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Actor
+        exclude = ['birth_year']
+
+
+class DetailedActorSerializer(serializers.ModelSerializer):
+
+    model = ''
+    class Meta:
+        model = Actor
+        fields = '__all__'
+
+
+
+class MovieSerializer(serializers.ModelSerializer):
+
+    actors = ActorSerializer(many=True)
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
+        depth = 1
+        # fields = ['id', 'name', 'release_year']
+        # exclude = ['actors', 'description']
--- movies_rest_app/views.py
+++ movies_rest_app/views.py
@@ -3,18 +3,26 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from movies_rest_app.models import *
+from movies_rest_app.serializers import MovieSerializer
 
 
 # Create your views here.
 
+# @api_view(['GET'])
+# def get_movies(request):
+#     query_set = Movie.objects.all()
+#     movies_list = []
+#     for movie in query_set:
+#         movies_list.append({
+#             'id': movie.pk,
+#             'name': movie.name,
+#             'description': movie.description
+#         })
+#     return Response(movies_list)
+
+
 @api_view(['GET'])
 def get_movies(request):
     query_set = Movie.objects.all()
-    movies_list = []
-    for movie in query_set:
-        movies_list.append({
-            'id': movie.pk,
-            'name': movie.name,
-            'description': movie.description
-        })
-    return Response(movies_list)
+    serializer = MovieSerializer(instance=query_set, many=True)
+    return Response(serializer.data)
