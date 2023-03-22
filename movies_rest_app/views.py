cca56a3aaceb4d311838c6048a6d3ab7d09da6d0
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2023-03-22 19:26:35 +0200

    initial commit

--- movies_rest_app/views.py
+++ movies_rest_app/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from movies_rest_app.models import *
+
 
 # Create your views here.
 
 @api_view(['GET'])
 def get_movies(request):
-    print(request)
-    return Response({'moves': 'my_movies'})
+    query_set = Movie.objects.all()
+    movies_list = []
+    for movie in query_set:
+        movies_list.append({
+            'id': movie.pk,
+            'name': movie.name,
+            'description': movie.description
+        })
+    return Response(movies_list)
